3894cdee10f59a3ad3546ce18fa0edecfeccbd64
Weslley <weslley.washington01486@gmail.com>
2023-07-06 17:17:56 -0300

    creating first project with React Native

---
+++ src/screens/components/Participant/index.tsx
@@ -0,0 +1,24 @@
+import { Text, TouchableOpacity, View } from 'react-native'
+
+import  {style}  from "./styles";
+
+type Props = {
+  name: string;
+  onRemove: () => void;
+}
+
+export function Participant({ name, onRemove }: Props) {
+  return (
+    <View style={style.container}>
+      <Text style={style.name}>
+        {name}
+      </Text>
+
+      <TouchableOpacity style={style.button} onPress={onRemove}>
+          <Text style={style.buttonText}>
+            -
+          </Text>
+        </TouchableOpacity>
+    </View>
+  )
+}
